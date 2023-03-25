13ba0e622885c1ed74a70a530b726cc184991f8e
54wb <1225768798@qq.com>
2023-03-25 20:04:26 +0800

    cancel wandb vis

--- configs/rtmdet/rtmdet_l_syncbn_fast_b4-36e_tzb-ship.py
+++ configs/rtmdet/rtmdet_l_syncbn_fast_b4-36e_tzb-ship.py
@@ -259,6 +259,3 @@ train_cfg = dict(
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
-visualizer = dict(
-    vis_backends = [dict(type='LocalVisBackend'), 
-                    dict(type='WandbVisBackend')])
